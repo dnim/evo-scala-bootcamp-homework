e81e4373971ea61437813ff02b51cf150e60060d
Siarhei Korsik <skorsik@evolutiongaming.com>
2020-09-25 10:17:44 +0300

    Basics homework

--- src/main/scala/com/evolution/bootcamp/homework/basics/Basics.scala
+++ src/main/scala/com/evolution/bootcamp/homework/basics/Basics.scala
@@ -6,10 +6,6 @@ object Basics extends App {
   // Homework. Implement functions that calculate https://en.wikipedia.org/wiki/Lowest_common_denominator and
   // https://en.wikipedia.org/wiki/Greatest_common_divisor for integers.
 
-  def lcm(a: Int, b: Int): Int =
-    if (a == 0 || b == 0) 0
-    else a / gcd(a, b) * b
-
   def gcd(a: Int, b: Int): Int = {
     if (a == 0 || b == 0) 0
     else {
@@ -17,10 +13,14 @@ object Basics extends App {
       @tailrec
       def findGcd(result: Int): Int = {
         if (result == 1) result
-        else if (a % result == 0 && b % result == 0) result
+        else if ((a % result, b % result) == (0, 0)) result
         else findGcd(result - 1)
       }
       findGcd(minValue)
     }
   }
+
+  def lcm(a: Int, b: Int): Int =
+    if (a == 0 || b == 0) 0
+    else a / gcd(a, b) * b
 }
